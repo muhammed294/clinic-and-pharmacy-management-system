2ff2ca69a1ee6a3abdf80a7e1c38ac4377aa5d08
MUhammed <muhammed340111@gmail.com>
2026-08-27 06:11:48 +0300

    authentication and authorization checking added

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
-router.get('/', patientController.getAllPatients);
-router.post('/', patientController.createPatient);
-router.get('/:id', patientController.getPatientByID);
-router.put('/:id', patientController.updatePatientByID);
-router.delete('/:id', patientController.deletePatientByID);
+router.get('/', authorizeRoles('admin', 'card_officer', 'doctor', 'lab_technician'), patientController.getAllPatients);
+router.get('/:id', authorizeRoles('admin', 'card_officer', 'doctor', 'lab_technician'), patientController.getPatientByID);
+router.post('/', authorizeRoles('admin', 'card_officer'), patientController.createPatient);
+router.put('/:id', authorizeRoles('admin', 'card_officer'), patientController.updatePatientByID);
+router.delete('/:id', authorizeRoles('admin', 'card_officer'), patientController.deletePatientByID);
 
 module.exports = router;
