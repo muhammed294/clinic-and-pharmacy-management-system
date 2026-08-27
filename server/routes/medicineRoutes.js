f9b31c59b3af35eba7cced94b99021e847fd813c
MUhammed <muhammed340111@gmail.com>
2026-08-27 06:12:08 +0300

    authentication and authorization added

--- server/routes/medicineRoutes.js
+++ server/routes/medicineRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const medicineController = require('../controllers/medicineController');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
-router.get('/', medicineController.getAllMedicines);
-router.post('/', medicineController.createMedicine);
-router.get('/:id', medicineController.getMedicineByID);
-router.put('/:id', medicineController.updateMedicineByID);
-router.delete('/:id', medicineController.deleteMedicineByID);
+router.get('/', authorizeRoles('admin', 'pharmacist'), medicineController.getAllMedicines);
+router.get('/:id', authorizeRoles('admin', 'pharmacist'), medicineController.getMedicineByID);
+router.post('/', authorizeRoles('admin', 'pharmacist'), medicineController.createMedicine);
+router.put('/:id', authorizeRoles('admin', 'pharmacist'), medicineController.updateMedicineByID);
+router.delete('/:id', authorizeRoles('admin', 'pharmacist'), medicineController.deleteMedicineByID);
 
 module.exports = router;
