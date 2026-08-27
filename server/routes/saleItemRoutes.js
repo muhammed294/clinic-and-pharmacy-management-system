8f20cb0bc7841250e4407cf529448e0e95704be5
MUhammed <muhammed340111@gmail.com>
2026-08-27 06:14:31 +0300

    authentication and authorization added

--- server/routes/saleItemRoutes.js
+++ server/routes/saleItemRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const saleItemController = require('../controllers/saleItemController');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
-router.post('/', saleItemController.createSaleItem);
-router.get('/', saleItemController.getAllSaleItem);
-router.get('/:id', saleItemController.getSaleItemByID);
-router.put('/:id', saleItemController.updateSaleItemByID);
-router.delete('/:id', saleItemController.deleteSaleItemByID);
+router.post('/', authorizeRoles('admin', 'pharmacist'), saleItemController.createSaleItem);
+router.get('/', authorizeRoles('admin', 'pharmacist'), saleItemController.getAllSaleItem);
+router.get('/:id', authorizeRoles('admin', 'pharmacist'), saleItemController.getSaleItemByID);
+router.put('/:id', authorizeRoles('admin', 'pharmacist'), saleItemController.updateSaleItemByID);
+router.delete('/:id', authorizeRoles('admin', 'pharmacist'), saleItemController.deleteSaleItemByID);
 
 module.exports = router;
