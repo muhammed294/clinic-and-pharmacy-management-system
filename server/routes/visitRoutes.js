cfa281f5714d3480e793ef90cf7d2512995c03d6
MUhammed <muhammed340111@gmail.com>
2026-08-27 06:15:02 +0300

    authentication and authorization added

--- server/routes/visitRoutes.js
+++ server/routes/visitRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const visitController = require('../controllers/visitController');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
-router.get('/', visitController.getAllVsits);
-router.post('/', visitController.createVisit);
-router.get('/:id', visitController.getVisitByID);
-router.put('/:id', visitController.updateVisitByID);
-router.delete('/:id', visitController.deleteVisitByID);
+router.get('/', authorizeRoles('admin', 'card_officer', 'doctor', 'lab_technician'), visitController.getAllVsits);
+router.get('/:id', authorizeRoles('admin', 'card_officer', 'doctor', 'lab_technician'), visitController.getVisitByID);
+router.post('/', authorizeRoles('admin', 'card_officer'), visitController.createVisit);
+router.put('/:id', authorizeRoles('admin', 'card_officer', 'doctor'), visitController.updateVisitByID);
+router.delete('/:id', authorizeRoles('admin'), visitController.deleteVisitByID);
 
 module.exports = router;
