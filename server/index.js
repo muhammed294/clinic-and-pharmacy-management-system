51422df9007062a2d88176c78ea89b018da5d026
MUhammed <muhammed340111@gmail.com>
2026-08-27 06:15:28 +0300

    authentication and authorization added

--- server/index.js
+++ server/index.js
@@ -7,32 +7,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authenticateToken = require('./middleware/authMiddleware');
+
+//Routes
 const medicineRoutes = require('./routes/medicineRoutes');
-app.use('/medicines', medicineRoutes);
+app.use('/medicines',authenticateToken, medicineRoutes);
 
 const stockRoutes = require('./routes/stockRoutes');
-app.use('/stock', stockRoutes);
+app.use('/stock',authenticateToken, stockRoutes);
 
 const pharmacySaleRoutes = require('./routes/pharmacySaleRoutes');
-app.use('/pharmacySale', pharmacySaleRoutes);
+app.use('/pharmacySale',authenticateToken, pharmacySaleRoutes);
 
 const saleItemRoutes = require('./routes/saleItemRoutes');
-app.use('/saleItem', saleItemRoutes);
+app.use('/saleItem',authenticateToken, saleItemRoutes);
 
 const patientRoutes = require('./routes/patientRoutes');
-app.use('/patient', patientRoutes);
+app.use('/patient',authenticateToken, patientRoutes);
 
 const doctorRoutes = require('./routes/doctorRoutes');
-app.use('/doctor', doctorRoutes);
+app.use('/doctor',authenticateToken, doctorRoutes);
 
 const visitRoutes = require('./routes/visitRoutes');
-app.use('/visit', visitRoutes);
+app.use('/visit',authenticateToken, visitRoutes);
 
 const vitalsRoutes = require('./routes/vitalsRoutes');
-app.use('/vitals', vitalsRoutes);
+app.use('/vitals',authenticateToken, vitalsRoutes);
 
 const prescriptionRoutes = require('./routes/prescriptionRoutes');
-app.use('/prescription', prescriptionRoutes);
+app.use('/prescription',authenticateToken, prescriptionRoutes);
+
+const labRequestRoutes = require('./routes/labRequestRoutes');
+app.use('/labrequest',authenticateToken, labRequestRoutes);
+
+const labResultRoutes = require('./routes/labResultRoutes');
+app.use('/labresult',authenticateToken, labResultRoutes);
+
+const paymentRoutes = require('./routes/paymentRoutes');
+app.use('/payment',authenticateToken, paymentRoutes);
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Clinic & Pharmacy API is running');
