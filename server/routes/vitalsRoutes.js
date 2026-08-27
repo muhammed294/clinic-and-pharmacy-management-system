08412ebf88f4125e6d30d5d75901db41823041c1
MUhammed <muhammed340111@gmail.com>
2026-08-27 06:15:16 +0300

    authentication and authorization added

--- server/routes/vitalsRoutes.js
+++ server/routes/vitalsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const vitalsController = require('../controllers/vitalsController');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
-router.get('/', vitalsController.getAllVitals);
-router.post('/', vitalsController.createVitals);
-router.get('/:id', vitalsController.getVitalsByID);
-router.put('/:id', vitalsController.updateVitalsByID);
-router.delete('/:id', vitalsController.deleteVitalsByID);
+router.get('/', authorizeRoles('admin', 'card_officer', 'doctor'), vitalsController.getAllVitals);
+router.get('/:id', authorizeRoles('admin', 'card_officer', 'doctor'), vitalsController.getVitalsByID);
+router.post('/', authorizeRoles('admin', 'card_officer'), vitalsController.createVitals);
+router.put('/:id', authorizeRoles('admin', 'card_officer'), vitalsController.updateVitalsByID);
+router.delete('/:id', authorizeRoles('admin'), vitalsController.deleteVitalsByID);
 
 module.exports = router;
