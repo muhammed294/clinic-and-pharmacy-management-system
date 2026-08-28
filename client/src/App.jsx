72cfed590477ddad690144bf82264ce5dc81f4a7
MUhammed <muhammed340111@gmail.com>
2026-08-28 06:19:57 +0300

    Frontend react configuration added

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import Header from './components/Header'
+import NavigationBar from './components/SampleHeader'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      {/* <NavigationBar/> */}
+      <Home/>
+    </>
+  )
+}
+
+export default App
