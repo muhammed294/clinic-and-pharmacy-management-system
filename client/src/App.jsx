11c2a959ada99e8a8a6717c18ca7ab649c4b1666
MUhammed <muhammed340111@gmail.com>
2026-08-30 06:29:37 +0300

    Updated version of app file with routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import NavigationBar from './components/SampleHeader'
 import Home from './pages/Home'
+import About from './pages/About'
+import Services from './components/home/Services'
+import Footer from './components/Footer'
+import Contact from './pages/Contact'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
       <Header/>
-      {/* <NavigationBar/> */}
-      <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      <Footer/>
     </>
   )
 }
