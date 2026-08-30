9bbcf8a889761e4744282ef607d4770e9ae449ac
MUhammed <muhammed340111@gmail.com>
2026-08-30 06:28:15 +0300

    href="" chnaged to Link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,20 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { HeartPulse } from 'react-bootstrap-icons';
+import {Link} from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 export default function Header() {
+    const { user } = useAuth();
+    console.log('current user:', user);
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="#" className='fw-bold fs-2' style={{color: 'blue'}}>Dr. Mulugeta</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className='fw-bold fs-2 d-flex align-items-center gap-2 text-primary'>
+            <HeartPulse size = {25}/>
+              Dr. Mulugeta
+        </Navbar.Brand>
         
         <Navbar.Toggle aria-controls="navbarScroll" />
         
@@ -16,10 +24,10 @@ export default function Header() {
           <Nav
             navbarScroll
           >
-            <Nav.Link href="#" className='text-primary  fs-5'>Home</Nav.Link>
-            <Nav.Link href="#" className='text-primary  fs-5'>About</Nav.Link>
-            <Nav.Link href="#" className='text-primary  fs-5'>Contact</Nav.Link>
-            <Button variant='outline-primary' className='fw-bold fs-5 ms-4'>login</Button>
+            <Nav.Link as={Link} to="/" className='text-primary  fs-5'>Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" className='text-primary  fs-5'>About</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className='text-primary  fs-5'>Contact</Nav.Link>
+            <Button as={Link} to="/login" variant='outline-primary' className='fw-bold fs-5 ms-4'>login</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
