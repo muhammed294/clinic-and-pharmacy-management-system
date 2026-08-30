969ea948ed930471e4f5a7a0ee8791c8a5b8be99
MUhammed <muhammed340111@gmail.com>
2026-08-30 06:24:59 +0300

    Home sections included

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/home/Hero'
+import Services from '../components/home/Services'
+import WhyChooseUs from '../components/home/WhyChooseUs'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Hero/>
+      <Services/>
+      <WhyChooseUs/>
+    </>
+  )
+}
