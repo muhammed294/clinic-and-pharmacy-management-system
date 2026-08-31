7ec5f0da7716bafb6ccd7b49a630b05907aeb6d7
MUhammed <muhammed340111@gmail.com>
2026-08-31 06:26:01 +0300

    fixing error on setUser()

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -13,6 +13,7 @@ export function AuthProvider({children}) {
     const login = (userData) => {
         localStorage.setItem('token', userData.token);
         localStorage.setItem('user', JSON.stringify(userData));
+        setUser(userData);
     };
 
     const logout = () => {
