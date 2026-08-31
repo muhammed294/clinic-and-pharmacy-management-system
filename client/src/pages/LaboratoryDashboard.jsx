968a8dea8d5297dc4129f8317394c328900990cf
MUhammed <muhammed340111@gmail.com>
2026-08-31 06:27:45 +0300

    Basic Laboratory dashboard added

---
+++ client/src/pages/LaboratoryDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext';
+
+export default function LaboratoryDashboard() {
+    const { user } = useAuth();
+
+    return (
+        <Container className='py-5'>
+            <h1 className='fw-bold text-primary'>Laboratory Dashboard</h1>
+            <p>Welcome, {user?.full_name}. You are logged in as {user?.role}.</p>
+        </Container>
+    );
+}
