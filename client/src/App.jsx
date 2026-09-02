38a04b45a551111bcd943a50767cb094710868bf
MUhammed <muhammed340111@gmail.com>
2026-09-02 15:17:22 +0300

    page components imported

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './components/home/Services'
-import Footer from './components/Footer'
-import Contact from './pages/Contact'
-import Login from './components/Login'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './components/home/Services';
+import Footer from './components/Footer';
+import Contact from './pages/Contact';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminDashboard from './pages/AdminDashboard';
+import PharmacyDashboard from './pages/PharmacyDashboard';
+import CardOfficerDashboard from './pages/CardOfficerDashboard';
+import DoctorsDashboard from './pages/DoctorsDashboard';
+import LaboratoryDashboard from './pages/LaboratoryDashboard';
 
 function App() {
 
@@ -18,6 +24,46 @@ function App() {
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/login' element={<Login/>}/>
+          <Route
+            path='/dashboard/admin'
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/dashboard/pharmacist'
+            element={
+              <ProtectedRoute allowedRoles={['admin', 'pharmacist']}>
+                <PharmacyDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/dashboard/card_officer'
+            element={
+              <ProtectedRoute allowedRoles={['admin', 'card_officer']}>
+                <CardOfficerDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/dashboard/doctor'
+            element={
+              <ProtectedRoute allowedRoles={['admin', 'doctor']}>
+                <DoctorsDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/dashboard/lab_technician'
+            element={
+              <ProtectedRoute allowedRoles={['admin', 'lab_technician']}>
+                <LaboratoryDashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       <Footer/>
     </>
